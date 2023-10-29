d1c5e3073dbad880648cedd6638f4f744ec54a53
safaALnuaymi <safashab98@gmail.com>
2023-10-29 19:45:57 +0300

    initial Commit

--- app/src/main/java/com/example/mvvmtodoapp/ui/theme/todo_list/TodoListViewModel.kt
+++ app/src/main/java/com/example/mvvmtodoapp/ui/theme/todo_list/TodoListViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.mvvmtodoapp.ui.theme.todo_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmtodoapp.data.Todo
 import com.example.mvvmtodoapp.data.TodoRepository
+import com.example.mvvmtodoapp.util.Routes
+import com.example.mvvmtodoapp.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +17,59 @@ class TodoListViewModel@Inject constructor(
     private val repository: TodoRepository
 ): ViewModel(){
 
+     val todos = repository.getTodos()
 
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    private var deletedTodo: Todo? = null
+
+
+    fun onEvent(event: TodoListEvent){
+        when(event) {
+            is TodoListEvent.OnTodoClick -> {
+                sentUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO + "?todoId=${event.todo.id}"))
+
+            }
+            is TodoListEvent.OnAddTodoClick -> {
+                sentUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO))
+
+            }
+            is TodoListEvent.OnUndoDeleteClick -> {
+                deletedTodo?.let { todo ->
+                    viewModelScope.launch {
+                        repository.insertTodo(todo)
+                    }
+                }
+
+            }
+            is TodoListEvent.OnDeleteTodoClick -> {
+                viewModelScope.launch {
+                    deletedTodo = event.todo
+                    repository.deleteTodo(event.todo)
+                    sentUiEvent(UiEvent.ShowSnackbar(
+                        message = "todo deleted",
+                        action = "Undo"
+                    ))
+                }
+
+            }
+            is TodoListEvent.OnDoneChange -> {
+                viewModelScope.launch {
+                    repository.insertTodo(
+                        event.todo.copy(
+                            isDone = event.isDone
+                        )
+                    )
+                }
+
+            }
+        }
+    }
+    private fun sentUiEvent(event: UiEvent){
+        viewModelScope.launch {
+            _uiEvent.send(event)
+
+        }
+    }
 }
